4f97e0f151fd63f959d37aa640b83b0076052f41
Vishrut Agrawal <zvishrut@gmail.com>
2024-05-17 23:34:55 -0500

    ignoring warning

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/custom/header";
 
 export default function Layout({children}: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen w-full flex-col">
+    <div className="flex w-full flex-col">
       <Header/>
       <div className="p-4 md:p-8">
         {children}
