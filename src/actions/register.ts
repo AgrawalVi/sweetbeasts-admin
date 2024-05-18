d53bb919f342e5b4c886c389e4f87a6842a70be9
Vishrut Agrawal <zvishrut@gmail.com>
2024-05-18 15:46:45 -0500

    register user working

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,7 +1,12 @@
 'use server'
 
 import * as z from "zod"
+import bcrypt from "bcrypt"
+import { db } from "@/lib/db"
+
 import { RegisterSchema } from "@/schemas"
+import { getUserByEmail } from "@/businessLogic/user"
+
 
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -10,5 +15,27 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validatedFields.success) {
     return { error: "Invalid Fields" };
   }
-  return { success: "Email Sent!" }
+
+  const { email, password, firstName, lastName} = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // make sure email is not taken
+  const existingUser = await getUserByEmail(email);
+  
+  if (existingUser) {
+    return { error: "Email already taken!" };
+  }
+
+  await db.user.create({
+    data: {
+      firstName, 
+      lastName,
+      email,
+      password: hashedPassword
+    }
+  })
+
+  // TODO: SEND VERIFICATION TOKEN EMAIL
+
+  return { success: "User created!" }
 }
