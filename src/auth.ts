38d4f67d77702f79c352a62b4751b263722f9796
Vishrut Agrawal <zvishrut@gmail.com>
2024-05-18 18:38:44 -0500

    added admin and customer roles to accounts

--- src/auth.ts
+++ src/auth.ts
@@ -5,6 +5,20 @@ import { db } from "@/lib/db"
 import authConfig from "./auth.config"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+    callbacks: {
+        async session({ token, session })  {
+            console.log({sessionToken: token})
+            if (token.sub && session.user) {
+                session.user.id = token.sub;
+
+            }
+            return session
+        },
+        async jwt({ token }) {
+            console.log(token);
+            return token;
+        },
+    },
     adapter: PrismaAdapter(db),
     session: { strategy: "jwt"},
     ...authConfig,
