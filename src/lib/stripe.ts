69fe49347887f06baa31bc156e9492cedb29737a
Vishrut Agrawal <zvishrut@gmail.com>
2024-06-11 19:46:26 -0500

    created edit product action

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,12 +1,7 @@
 import { Stripe } from 'stripe'
 
-const API_KEY = process.env.STRIPE_SECRET_KEY
-
-if (!API_KEY) {
-  console.error('Missing Stripe secret key')
-  process.exit(1)
-}
+const API_KEY = process.env.STRIPE_SECRET_KEY!
 
 export const stripe = new Stripe(API_KEY, {
-  apiVersion: '2024-04-10; custom_checkout_beta=v1' as any,
+  apiVersion: '2024-04-10',
 })
