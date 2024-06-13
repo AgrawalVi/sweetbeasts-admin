bf142396f9ffed859a7d61e75ee0355ab3bd0cbd
Vishrut Agrawal <zvishrut@gmail.com>
2024-06-12 19:37:04 -0500

    added exit blocker

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,41 +1,11 @@
 'use client'
 
-import { admin } from '@/actions/auth/admin'
-import { RoleGate } from '@/components/auth/role-gate'
-import { Button } from '@/components/ui/button'
-import { useCurrentRole } from '@/hooks/use-current-role'
-import { UserRole } from '@prisma/client'
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog'
-
-import { Input } from '@/components/ui/input'
-import CreateProductForm from '@/components/admin/create-product-form'
+import CreateProductButton from '@/components/admin/products/create-product-button'
 
 export default function Products() {
-  const currentRole = useCurrentRole()
-
   return (
     <>
-      <div>
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button> Add Product</Button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>Add Product</DialogHeader>
-            <DialogDescription>
-              Fill out the form to add a product
-            </DialogDescription>
-            <CreateProductForm />
-          </DialogContent>
-        </Dialog>
-      </div>
+      <CreateProductButton />
     </>
   )
 }
