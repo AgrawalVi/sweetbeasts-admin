eeb747464632b2a6270de647d13bc183625446ab
Vishrut Agrawal <zvishrut@gmail.com>
2024-06-17 18:03:26 +0200

    changed error messages

--- src/actions/orders/new-order.ts
+++ src/actions/orders/new-order.ts
@@ -1,12 +1,14 @@
-import { db } from "@/lib/db";
-import { stripe } from "@/lib/stripe";
-import Stripe from "stripe";
+import { db } from '@/lib/db'
+import { stripe } from '@/lib/stripe'
+import Stripe from 'stripe'
 
-export const createOrder = async (event: Stripe.CheckoutSessionCompletedEvent) => {
-  const checkoutSession = event.data.object;
+export const createOrder = async (
+  event: Stripe.CheckoutSessionCompletedEvent,
+) => {
+  const checkoutSession = event.data.object
 
   // get order information from event
-  const timePlaced = new Date(event.created * 1000);
+  const timePlaced = new Date(event.created * 1000)
 
   // need to get line items from a checkout session and decrease the quantity in our database by 1
   // create order in database
