1c6cce75ad6b6647559c830cf13e5a3886c911d5
Vishrut Agrawal <zvishrut@gmail.com>
2024-08-13 17:08:31 -0500

    added trpc logic to the project

---
+++ src/server/api/routers/orders.ts
@@ -0,0 +1,8 @@
+import { createTRPCRouter, publicProcedure } from '@/server/api/trpc'
+import { db } from '@/lib/db'
+
+export const orderRouter = createTRPCRouter({
+  getAllOrders: publicProcedure.query(async () => {
+    return await db.order.findMany();
+  })
+})
